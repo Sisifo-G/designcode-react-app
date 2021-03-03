2742261b07d6df9396dc451f329acc53c7cb9b31
Sisifo-G <manuelperdomo1710@icloud.com>
2021-03-02 20:30:15 -0500

    Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,16 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to</h1>
-    <p>design code React Apps</p>
-    <p>
-      Complete courses about the best tools and design systems. Prototype and
-      build apps with React and Swift build
-    </p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design code React Apps</h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift build
+        </p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </div>
 )
 
